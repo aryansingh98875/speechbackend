074b81bf2c6b0893a3400e1cb3bb47b297df7bdd
aryansingh98875 <aryansingh98875@gmail.com>
2024-11-24 03:48:38 +0530

    chsnges in db config

--- dist/lib/db.js
+++ dist/lib/db.js
@@ -1,12 +1,5 @@
 "use strict";
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-//
-const prismaClientSingleton = () => {
-    return new client_1.PrismaClient();
-};
-const db = (_a = globalThis.prismaGlobal) !== null && _a !== void 0 ? _a : prismaClientSingleton();
+const db = new client_1.PrismaClient();
 exports.default = db;
-if (process.env.NODE_ENV !== 'production')
-    globalThis.prismaGlobal = db;
